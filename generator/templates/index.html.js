24f110d77745424f313f5f85f4007bf1d57c79fb
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-08-05 11:11:00 -0500

    Update index.html.js

--- generator/templates/index.html.js
+++ generator/templates/index.html.js
@@ -8,6 +8,7 @@ module.exports = ({ meta }) =>
   <title>${meta.title}</title>
   <meta name="description" content="${meta.description}" />
   <meta property="og:title" content="${meta.title}" />
+  <meta property="og:type" content="website" />
   <meta property="og:url" content="${meta.url}" />
   <meta property="og:description" content="${meta.description}" />
   <meta property="og:image" content="${meta.image}" />
